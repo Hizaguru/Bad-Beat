39ef936e810103a521ced93316da0994b518bf87
Hizaguru <jukka-pekka.lappalainen@outlook.com>
2023-06-04 13:31:03 +0300

    Remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import HandRange from "./Components/HandRangeChart/HandRange";
 import PotOddsCalculator from "./Components/PotOdds/PotOddsCalculator";
 import TabPanel from "./Components/TabPanel";
 
-
 function a11yProps(index: number) {
   return {
     id: `vertical-tab-${index}`,
@@ -25,47 +24,25 @@ export default function App() {
     <Box
       sx={{
         flexGrow: 1,
-      
         display: "flex",
         height: 224,
       }}
     >
       <Tabs
         orientation="vertical"
-    
         value={value}
         onChange={handleChange}
         aria-label="Vertical tabs example"
-        sx={{ borderRight: 1, borderColor: "divider", height:224 }}
+        sx={{ borderRight: 1, borderColor: "divider", height: 224 }}
       >
         <Tab label="Pot Odds" {...a11yProps(0)} />
         <Tab label="Hand Range Chart" {...a11yProps(1)} />
-        <Tab label="Item Three" {...a11yProps(2)} />
-        <Tab label="Item Four" {...a11yProps(3)} />
-        <Tab label="Item Five" {...a11yProps(4)} />
-        <Tab label="Item Six" {...a11yProps(5)} />
-        <Tab label="Item Seven" {...a11yProps(6)} />
       </Tabs>
       <TabPanel value={value} index={0}>
-        <PotOddsCalculator/>
+        <PotOddsCalculator />
       </TabPanel>
       <TabPanel value={value} index={1}>
-        <HandRange/>
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        Item Three
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-        Item Four
-      </TabPanel>
-      <TabPanel value={value} index={4}>
-        Item Five
-      </TabPanel>
-      <TabPanel value={value} index={5}>
-        Item Six
-      </TabPanel>
-      <TabPanel value={value} index={6}>
-        Item Seven
+        <HandRange />
       </TabPanel>
     </Box>
   );
