ccc23d6af9f6b313f40a2123974ed06fc0ddb259
Hizaguru <jukka-pekka.lappalainen@outlook.com>
2023-06-09 15:53:46 +0300

    remove unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -40,11 +40,6 @@ export default function App() {
       >
         <Tab label="Pot Odds" {...a11yProps(0)} />
         <Tab label="Hand Range Chart" {...a11yProps(1)} />
-        <Tab label="Item Three" {...a11yProps(2)} />
-        <Tab label="Item Four" {...a11yProps(3)} />
-        <Tab label="Item Five" {...a11yProps(4)} />
-        <Tab label="Item Six" {...a11yProps(5)} />
-        <Tab label="Item Seven" {...a11yProps(6)} />
       </Tabs>
       <TabPanel value={value} index={0}>
         <PotOddsCalculator/>
@@ -52,21 +47,7 @@ export default function App() {
       <TabPanel value={value} index={1}>
         <HandRange/>
       </TabPanel>
-      <TabPanel value={value} index={2}>
-        Item Three
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-        Item Four
-      </TabPanel>
-      <TabPanel value={value} index={4}>
-        Item Five
-      </TabPanel>
-      <TabPanel value={value} index={5}>
-        Item Six
-      </TabPanel>
-      <TabPanel value={value} index={6}>
-        Item Seven
-      </TabPanel>
+
     </Box>
   );
 }
